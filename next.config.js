e9ae6d1c6a400ac439ff63f78cbeb67b014849f3
Rohith Raj Bingi <rohithrajb@gmail.com>
2025-08-20 23:21:08 +0530

    change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+console.log(isProd)
+
 const nextConfig = {
+  basePath: isProd ? '/sarthak-final' : '',
+  assetPrefix: isProd ? '/sarthak-final/' : '',
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
