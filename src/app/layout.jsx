481ba9cc1399dfc80125d56714279790f2c8ee86
Rohith Raj Bingi <rohithrajb@gmail.com>
2025-08-21 00:38:19 +0530

    final

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -173,7 +173,7 @@ export default function RootLayout({ children }) {
         {/* Preload critical assets */}
         <link 
           rel="preload" 
-          href={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/my-profile.png`}
+          href="/sarthak-final/images/my-profile.jpg"
           as="image"
         />
         <title>Sarthak Hatwar | Data Engineer </title>
